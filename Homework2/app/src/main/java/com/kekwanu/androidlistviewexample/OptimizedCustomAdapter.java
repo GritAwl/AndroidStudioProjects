b4b13d51d4b274744e820eadc191773d816410af
Luke Bayler <lukebayler@gmail.com>
2014-10-28 14:50:40 -0700

    All state names are printed, and next to the state names are two squares, each with a randomly generated color. Colors weren't showing up because the alpha/transparency values had to be set to a number other than zero (0).
    The color of the squares is regenerated (randomly) each time the
    state ListView is scrolled. Is this within the bounds of the assignment?
    
    All that is left to do is to add alternating backgrounds to the rows.

--- Homework2/app/src/main/java/com/kekwanu/androidlistviewexample/OptimizedCustomAdapter.java
+++ Homework2/app/src/main/java/com/kekwanu/androidlistviewexample/OptimizedCustomAdapter.java
@@ -2,15 +2,16 @@ package com.kekwanu.androidlistviewexample;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by PSP502AP001 on 10/22/2014.
@@ -30,6 +31,11 @@ public class OptimizedCustomAdapter extends BaseAdapter {
     }
 
     //define the Holder pattern class
+
+    /**
+     * This is our ViewHolder.
+     * It is used to cache the TextView.
+     */
     static class ViewHolder{
         TextView state;
         View square1;
@@ -64,8 +70,14 @@ public class OptimizedCustomAdapter extends BaseAdapter {
                         "and position = " + position);
 
         ViewHolder viewHolder;
-        ListView rows = (ListView)findViewById(R.id.listview);
+//        ListView rows = (ListView)findViewById(R.id.listview);
 
+        /**
+         * The convertView parameter is a "scrap-view".
+         * It has a non-null value when the ListView is asking you to
+         * recycle the row layout, so when convertView is non-null, update
+         * its contents instead of inflating a new row layout.
+         */
         if (convertView == null){ //the first time around, the view is null, so inflate it
 
             //inflate using the system inflater. This returns a reference to the inflater,
@@ -85,24 +97,57 @@ public class OptimizedCustomAdapter extends BaseAdapter {
             viewHolder.state = (TextView) convertView.findViewById(R.id.state);
             viewHolder.square1 = (View) convertView.findViewById(R.id.square1);
             viewHolder.square2 = (View) convertView.findViewById(R.id.square2);
+
+            //  Store the ViewHolder with the View.
             convertView.setTag(viewHolder);
-
-
-
-
-            if (position % 2 == 0) {
-                rows.findViewById(position).setBackgroundColor(0x33FF0000);
-            } else {
-                rows.findViewById(position).setBackgroundColor(0x33FFFF00);
-            }
-
-
         }
+
+        /**
+         * We avoided calling findViewById() on the resource,
+         * so just use the ViewHolder.
+         */
+
+        viewHolder = (ViewHolder) convertView.getTag();
+        viewHolder.state.setText(getItem(position));
 
         ((ViewHolder) convertView.getTag()).state.setText(getItem(position));
 
-
+        /**
+         * TODO:
+         * The background color of the squares is changed every time the
+         * ListView scrolls. Is this within the bounds of the project specifications?
+         */
+        ((ViewHolder) convertView.getTag()).square1.setBackgroundColor(makeRandomColor());
+        ((ViewHolder) convertView.getTag()).square2.setBackgroundColor(makeRandomColor());
 
         return convertView;
+    }
+
+    /**
+     * This method randomly generates an alpha/transparency value along with an RGB value.
+     * @return The integer color value in AARRGGBB format.
+     */
+    static int makeRandomColor() {
+        String TAG = "HW2";
+        Random r = new Random();
+
+        int AA = r.nextInt(255);
+        Log.i(TAG, "AA = " + AA);
+
+        float[] RGB = new float[3];
+
+        RGB[0] = r.nextInt(255);
+        Log.i(TAG, "RR = " + RGB[0]);
+
+        RGB[1] = r.nextInt(255);
+        Log.i(TAG, "GG = " + RGB[1]);
+
+        RGB[2] = r.nextInt(255);
+        Log.i(TAG, "BB = " + RGB[2]);
+
+        int AARRGGBB = Color.HSVToColor(AA, RGB);
+        Log.i(TAG, "AARRGGBB = " + AARRGGBB);
+
+        return AARRGGBB;
     }
 }
